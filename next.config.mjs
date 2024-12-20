a25c9cbc4d4a8b2f460eb2123f2cf102a6c3c106
theodaguier <theo.daguier@icloud.com>
2024-12-20 14:11:48 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          // { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, private' },
+          // { key: 'Pragma', value: 'no-cache' },
+          {
+            key: 'Netlify-Vary',
+            value: 'query=sort|page|per_page',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
