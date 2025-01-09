1936dfdf68610f5a3c4f54f21b28c167b5093e45
theodaguier <theo.daguier@icloud.com>
2025-01-09 15:02:44 +0100

    adding sub categories images

--- src/collections/SubCategories.ts
+++ src/collections/SubCategories.ts
@@ -14,5 +14,31 @@ export const SubCategories: CollectionConfig = {
       type: 'text',
       required: true,
     },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      admin: {
+        description: 'Image de la sous-catégorie',
+      },
+    },
   ],
+  hooks: {
+    afterRead: [
+      async ({ doc, req }) => {
+        if (doc.image && typeof doc.image === 'string') {
+          const payload = req.payload
+          const image = await payload.findByID({
+            collection: 'media',
+            id: doc.image,
+          })
+          return {
+            ...doc,
+            image,
+          }
+        }
+        return doc
+      },
+    ],
+  },
 }
